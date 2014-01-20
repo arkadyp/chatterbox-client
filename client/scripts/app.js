2465b66c0acdaa348c405db473af5c6124b4d369
arkadyp <arkady.pevzner@gmail.com>
2014-01-20 11:59:48 -0800

    Adds div in html for chat messages; adds ajax request to GET chat messages and console log the data object

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,17 @@
-// YOUR CODE HERE:
+var room = {'roomname':'lobby'};
+
+$.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  data: JSON.stringify(room),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+    console.log(data);
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
