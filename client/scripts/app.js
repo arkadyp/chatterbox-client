0b5903e6ad29302c10b8c1899794782f3ffaba41
arkadyp <arkady.pevzner@gmail.com>
2014-01-20 12:08:12 -0800

    Adds function to console.log all username's and messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,14 @@
 var room = {'roomname':'lobby'};
 
+var displayMessage = function(data) {
+  _.each(data.results, function(message) {
+    var username = message.username || 'unknown';
+    var text = message.text || '';
+    console.log(username+': '+text);
+
+  });
+}
+
 $.ajax({
   // always use this url
   url: 'https://api.parse.com/1/classes/chatterbox',
@@ -7,11 +16,11 @@ $.ajax({
   data: JSON.stringify(room),
   contentType: 'application/json',
   success: function (data) {
-    console.log('chatterbox: Message sent');
-    console.log(data);
+    displayMessage(data);
   },
   error: function (data) {
     // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
     console.error('chatterbox: Failed to send message');
   }
 });
+
